2f30875538c6d367e18ff5c5ca97f5ea2bbc8bc1
Endcat <1013387275@qq.com>
2019-06-22 01:04:40 +0800

    add get fuzz func

--- CsharpSecurityTools/CsharpSecurityTools/Program.cs
+++ CsharpSecurityTools/CsharpSecurityTools/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Net;
+using System.IO;
 
 namespace CsharpSecurityTools
 {
@@ -10,8 +12,44 @@ namespace CsharpSecurityTools
 
           [DllImport("libc")]
           static extern void printf(string message);
+          static void GetParamFuzz(string[] args)
+          {
+               string url = args[0];
+               int index = url.IndexOf("?");
+               string[] parms = url.Remove(0, index + 1).Split('&');
+               foreach (string parm in parms)
+               {
+                    //Console.WriteLine(parm);
+                    string xssUrl = url.Replace(parm, parm + "fd<xss>sa"); // pollution
+                    string sqlUrl = url.Replace(parm, parm + "fd'sa");
+
+                    //Console.WriteLine(xssUrl);
+                    //Console.WriteLine(sqlUrl);
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(sqlUrl);
+                    request.Method = "GET";
+
+                    string sqlresp = string.Empty;
+                    using (StreamReader rdr = new StreamReader(request.GetResponse().GetResponseStream())) // dispose optimize
+                         sqlresp = rdr.ReadToEnd();
+
+                    request = (HttpWebRequest)WebRequest.Create(xssUrl);
+                    request.Method = "GET";
+                    string xssresp = string.Empty;
+
+                    using (StreamReader rdr = new StreamReader(request.GetResponse().GetResponseStream()))
+                         xssresp = rdr.ReadToEnd();
+
+                    if (xssresp.Contains("<xss>"))
+                         Console.WriteLine("Possible XSS point found in parameter: " + parm);
+
+                    if (sqlresp.Contains("error in your SQL syntax"))
+                         Console.WriteLine("SQL injection point found in parameter: " + parm);
+
+               }
+          }
           static void Main(string[] args)
           {
+               // use cmd params to replace arguments string array
                OperatingSystem os = Environment.OSVersion;
                if (os.Platform == PlatformID.Win32NT)
                {
@@ -22,6 +60,14 @@ namespace CsharpSecurityTools
                     printf("Hello Miao!");
                }
                //Console.WriteLine("Hello World!");
+
+               string[] arguments = new string[9];
+               arguments[0] = "http://192.168.241.128/cgi-bin/badstore.cgi?searchquery=hello&action=search&x=0&y=0";
+               GetParamFuzz(arguments);
+
           }
+
+          
      }
 }
+
