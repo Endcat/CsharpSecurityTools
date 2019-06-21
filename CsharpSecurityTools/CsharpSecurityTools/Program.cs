42fede3b38487757a1b807892237d916c109501b
Endcat <1013387275@qq.com>
2019-06-21 16:37:35 +0800

    p_invoke

---
+++ CsharpSecurityTools/CsharpSecurityTools/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CsharpSecurityTools
+{
+     class MainClass
+     {
+          [DllImport("user32", CharSet = CharSet.Auto)]
+          static extern int MessageBox(IntPtr hWnd, String text, String caption, int options);
+
+          [DllImport("libc")]
+          static extern void printf(string message);
+          static void Main(string[] args)
+          {
+               OperatingSystem os = Environment.OSVersion;
+               if (os.Platform == PlatformID.Win32NT)
+               {
+                    MessageBox(IntPtr.Zero, "Hello!", "Hello", 0);
+               }
+               else
+               {
+                    printf("Hello Miao!");
+               }
+               //Console.WriteLine("Hello World!");
+          }
+     }
+}
